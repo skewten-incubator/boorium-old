27325309143416e775c090c746d70e7df8bef9de
Ivan Kay <ivan@sq10.net>
2015-02-08 16:21:04 -0600

    finish titlebar buttons

---
+++ TODO.md
@@ -0,0 +1,4 @@
+todo
+===
+
+* On fullscreen enter, change the buttons so the fullscreen button disappears, and only the restore button is visible.
--- src/js/handlers/titlebar.js
+++ src/js/handlers/titlebar.js
@@ -5,6 +5,11 @@ function attach_handlers(){
 function attach_titlebar_buttons(){
     $("#titlebar .buttons .minimize").on("click", minimize_window);
     $("#titlebar .buttons .close").on("click", close_window);
+    $("#titlebar .buttons .maximize").on("click", maximize_window);
+    $("#titlebar .buttons .restore").on("click", restore_window);
+    $("#titlebar .buttons .fs").on("click", toggle_fullscreen);
+    win.on("maximize", function(){set_maximized(true)});
+    win.on("unmaximize", function(){set_maximized(false)});
 }
 
 function minimize_window(){
@@ -13,6 +18,28 @@ function minimize_window(){
 
 function close_window(){
     win.close();
+}
+
+var maximized = true;
+function set_maximized(max){
+    maximized = max;
+    var target = $("#titlebar .buttons .mr");
+    maximized && target.addClass("max");
+    !maximized && target.removeClass("max");
+}
+
+function maximize_window(){
+    win.maximize();
+    set_maximized(true);
+}
+
+function restore_window(){
+    win.unmaximize();
+    set_maximized(false);
+}
+
+function toggle_fullscreen(){
+    win.toggleFullscreen();
 }
 
 exports.attach = attach_handlers;
